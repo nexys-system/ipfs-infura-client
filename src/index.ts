9a088cc10d51dfaf377076a51ca45b555f9feb12
John B <johan@nexys.ch>
2022-01-26 14:47:38 -0800

    added client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-export default "hello world";
+import { create, CID, IPFSHTTPClient } from 'ipfs-http-client';
+
+const getHeaders = (username: string, password: string) => {
+  const authorization =
+    'basic ' + Buffer.from([username, password].join(':')).toString('base64');
+
+  return { authorization };
+};
+
+class SimplifiedInfuraIPFSClient {
+  client: IPFSHTTPClient;
+
+  constructor({
+    username,
+    password,
+    url
+  }: {
+    url: string;
+    username: string;
+    password: string;
+  }) {
+    const headers = getHeaders(username, password);
+    this.client = create({ url, headers });
+  }
+
+  set = async (s: string) => this.client.add(s);
+
+  get = async (cid: CID): Promise<string> =>
+    new Promise(async resolve => {
+      for await (const variable of this.client.cat(cid)) {
+        const b = Buffer.from(variable).toString('utf-8');
+        resolve(b);
+      }
+    });
+}
+
+export default SimplifiedInfuraIPFSClient;
